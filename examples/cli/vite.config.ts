104803503b814357f77fa97b1cd843672abf5617
chouchouji <1305974212@qq.com>
2025-09-25 22:01:42 +0800

    chore: clean tsconfig.json

--- examples/cli/vite.config.ts
+++ examples/cli/vite.config.ts
@@ -1,8 +1,8 @@
 import uni from '@dcloudio/vite-plugin-uni'
-import { defineConfig } from 'vite'
+import { defineConfig, Plugin } from 'vite'
 import componentPlaceholderPlugin from '../../dist/index'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni(), componentPlaceholderPlugin()],
+  plugins: [uni(), componentPlaceholderPlugin() as Plugin],
 })
